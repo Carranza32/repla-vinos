07335614be68d12a028d7fcf3f008b1d5b3fac63
Mario Ernesto Carranza <mario.carranza996@gmail.com>
2023-05-13 17:43:01 -0600

    recuperate

--- lib/screens/auth/recuperate_screen.dart
+++ lib/screens/auth/recuperate_screen.dart
@@ -14,6 +14,7 @@ class RecuperateScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		var size = MediaQuery.of(context).size;
+		var theme = Theme.of(context);
 
 		return GestureDetector(
 			onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
@@ -23,7 +24,7 @@ class RecuperateScreen extends StatelessWidget {
 				body: LayoutBuilder(
 					builder: (context, constraints) {
 						if (constraints.maxWidth > 600) {
-							return _buildLargeScreen(size);
+							return _buildLargeScreen(size, theme);
 						} else {
 							return _buildSmallScreen(size);
 						}
@@ -34,28 +35,63 @@ class RecuperateScreen extends StatelessWidget {
 	}
 
 	/// For large screens
-	Widget _buildLargeScreen(Size size) {
-			return Row(
-				children: [
-					Expanded(
-						flex: 4,
-						child: RotatedBox(
-							quarterTurns: 0,
-							child: Image.asset(
-								'assets/132033-green-login.gif',
-								height: size.height * 0.5,
-								width: double.infinity,
-								fit: BoxFit.contain,
+	Widget _buildLargeScreen(Size size, context) {		
+		return Container(
+			height: size.height,
+			width: size.width,
+			decoration: const BoxDecoration(
+				image: DecorationImage(
+					image: AssetImage('assets/Diseño_sin_título.png'),
+					fit: BoxFit.cover,
+					alignment: Alignment.topCenter,
+					opacity: 0.2
+				),
+			),
+			child: Container(
+				padding: EdgeInsets.symmetric(horizontal: size.width > 800 ? size.width * 0.09 : size.width * 0.001),
+				child: Row(
+					crossAxisAlignment: CrossAxisAlignment.stretch,
+					mainAxisSize: MainAxisSize.max,
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: [
+						Expanded(
+							flex: 4,
+							child: Column(
+								mainAxisAlignment: MainAxisAlignment.center,
+								children: [
+									Image.asset(
+										'assets/logoid2.png',
+										height: 140,
+										width: 400,
+									),
+
+									RotatedBox(
+										quarterTurns: 0,
+										child: Image.asset(
+											'assets/animation_500_lft97u10.gif',
+											height: size.height * 0.5,
+											width: double.infinity,
+											fit: BoxFit.contain,
+										),
+									),
+
+									Image.asset(
+										'assets/Logos_frase01bg.png',
+										height: 140,
+										width: 400,
+									),
+								],
 							),
 						),
-					),
-					SizedBox(width: size.width * 0.06),
-					Expanded(
-						flex: 5,
-						child: _buildMainBody(size),
-					),
-				],
-			);
+						SizedBox(width: size.width * 0.06),
+						Expanded(
+							flex: 5,
+							child: _buildMainWebBody(size),
+						),
+					],
+				),
+			),
+		);
 	}
 
 	/// For Small screens
@@ -158,14 +194,119 @@ class RecuperateScreen extends StatelessWidget {
 		);
 	}
 
+	/// Main Body for web
+	Widget _buildMainWebBody(Size size) {
+		return Column(
+			crossAxisAlignment: CrossAxisAlignment.start,
+			mainAxisAlignment: size.width > 600 ? MainAxisAlignment.center : MainAxisAlignment.start,
+			children: [
+				Center(
+					child: Text(
+						'BIENVENIDOS A REPLAVINOS',
+						style: TextStyle(
+							fontSize: size.height * 0.050,
+							fontWeight: FontWeight.bold,
+							color: Colors.black,
+
+						),
+					),
+				),
+
+				Center(
+					child: Text(
+						'Periodos de Resguardo de Plaguicidas en Uva Vinífera',
+						style: TextStyle(
+							fontSize: size.height * 0.020,
+							color: Colors.black,
+						),
+					),
+				),
+
+				SizedBox(
+					height: size.height * 0.03,
+				),
+				
+				const SizedBox(
+					height: 10,
+				),
+				SizedBox(
+					height: size.height * 0.03,
+				),
+				Container(
+					padding: const EdgeInsets.all(20),
+					decoration: BoxDecoration(
+						borderRadius: BorderRadius.circular(10),
+						color: const Color.fromARGB(172, 70, 97, 36),
+					),
+					child: Form(
+						key: _formKey,
+						autovalidateMode: AutovalidateMode.onUserInteraction,
+						child: Column(
+							children: [
+								Padding(
+									padding: const EdgeInsets.only(bottom: 5.0),
+									child: Text(
+										'recuperate'.tr,
+										style: kLoginTitleStyle(size),
+									),
+								),
+
+								TextFormField(
+									keyboardType: TextInputType.emailAddress,
+									style: kTextFormFieldStyle(),
+									decoration: authFormFieldStyle().copyWith(
+										prefixIcon: const Icon(Icons.person, color: Color(0xff4bbf78),),
+										labelText: 'email'.tr,
+										hintText: 'email_hint'.tr,
+										fillColor: Colors.white,
+										filled: true,
+									),
+									controller: authController.emailTextController,
+									validator: (value) => (value!.isMyEmail) ? null : 'email_wrong'.tr,
+								),
+								SizedBox(
+									height: size.height * 0.02,
+								),
+
+								SizedBox(
+									height: size.height * 0.03,
+								),
+
+								SizedBox(
+									height: size.height * 0.02,
+								),
+								Text(
+									'login_footer'.tr,
+									style: kLoginTermsAndPrivacyStyle(size),
+									textAlign: TextAlign.center,
+								),
+								SizedBox(
+									height: size.height * 0.02,
+								),
+
+								/// Login Button
+								loginButton(isWhite: true),
+								SizedBox(
+									height: size.height * 0.03,
+								),
+
+								loginButtonPage(isWhite: true),							
+							],
+						),
+					),
+				),
+			],
+		);
+	}
+
 	// Login Button
-	Widget loginButton() {
+	Widget loginButton({bool isWhite = false}) {
 		return SizedBox(
 			width: double.infinity,
 			height: 55,
 			child: ElevatedButton(
 				style: ElevatedButton.styleFrom(
-					backgroundColor: const Color(0xffa3fb82),
+					backgroundColor: (isWhite) ? Colors.white : const Color(0xffa3fb82),
 					shape: RoundedRectangleBorder(
 						borderRadius: BorderRadius.circular(15),
 					),
@@ -176,6 +317,31 @@ class RecuperateScreen extends StatelessWidget {
 					}
 				},
 				child: Text('recuperate_password'.tr, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color(0xff111b31))),
+			),
+		);
+	}
+
+	// Go to Login Page
+	Widget loginButtonPage({bool isWhite = false}) {
+		return SizedBox(
+			width: double.infinity,
+			height: 55,
+			child: ElevatedButton(
+				style: ElevatedButton.styleFrom(
+					backgroundColor: (isWhite) ? Colors.white : const Color(0xffa3fb82),
+					shape: RoundedRectangleBorder(
+						borderRadius: BorderRadius.circular(15),
+					),
+				),
+				onPressed: () {
+					Get.toNamed('login');
+
+					authController.emailTextController.clear();
+					authController.passwordTextController.clear();
+					
+					_formKey.currentState?.reset();
+				},
+				child: Text('return'.tr, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Color(0xff111b31))),
 			),
 		);
 	}
